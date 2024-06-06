4542843e4437ba58b55a4e464a6f1fdbeb08707a
rik <vslimrik@gmail.com>
2024-06-06 15:52:03 +0800

    Update style of link

--- fe/src/app/pages/layout.jsx
+++ fe/src/app/pages/layout.jsx
@@ -1,13 +1,28 @@
+"use client"
+
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export default function Layout({ children }) {
+  const pathname = usePathname()
+
   return (
     <div className="w-full bg-grey flex flex-col p-8">
       <nav className="flex flex-row gap-6">
-        <Link href="/pages/page1" className="homepage-link">
+        <Link
+          href="/pages/page1"
+          className={`homepage-link ${
+            pathname === "/pages/page1" ? "selected" : ""
+          }`}
+        >
           Page 1
         </Link>
-        <Link href="/pages/page2" className="homepage-link">
+        <Link
+          href="/pages/page2"
+          className={`homepage-link ${
+            pathname === "/pages/page2" ? "selected" : ""
+          }`}
+        >
           Page 2
         </Link>
       </nav>
